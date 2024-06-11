ef133216fb270c8e1c91dfc86fc36b560f3850f0
Shchekn <68048381+Shchekn@users.noreply.github.com>
2024-06-11 09:13:17 +0300

    Subqueries-and-Joins.sql

--- Subqueries-and-Joins.sql
+++ Subqueries-and-Joins.sql
@@ -436,7 +436,26 @@ FROM
 	AS [CurrencyRankingSubquery]
 	WHERE [CurrencyRank] = 1
 
+--second
 
+USE Geography;
+GO
+
+SELECT
+	c.ContinentCode
+	,c.CurrencyCode
+	,COUNT(c.CurrencyCode) AS CurrencyUsage
+FROM Countries AS c
+GROUP BY c.ContinentCode, c.CurrencyCode
+HAVING COUNT(c.CurrencyCode) > 1 AND 
+		COUNT(c.CurrencyCode) = (SELECT t.Usage FROM
+			(SELECT TOP 1
+				COUNT(*) AS Usage
+			FROM Countries AS dt
+			WHERE dt.ContinentCode = c.ContinentCode
+			GROUP BY dt.CurrencyCode
+			ORDER BY Usage DESC) AS t)
+ORDER BY c.ContinentCode;
 
 --16.	Countries Without Any Mountains
 --Create a query that returns the count of all countries, which don’t have a mountain.
